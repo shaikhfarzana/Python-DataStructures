b0c4f6e8b654d24dd4bb63c7b439af6352b33109
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-01-15 04:44:52 +0530

    [Farzana] Add: Python program to count the number of CPUs.

---
+++ Basic_Programs/13_CountCPU.py
@@ -0,0 +1,11 @@
+"""
+@Author: Farzana Shaikh
+@Date: 12-01-2022 02:32AM
+@Last Modified by: Farzana Shaikh
+@Last Modified time: 12-01-2022 02:45AM
+@Title : Basic Python(To find out the number of CPUs using.)
+"""
+
+
+import multiprocessing
+print(multiprocessing.cpu_count())
