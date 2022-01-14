ae134d779e7dad4c40eda4e67ddbb16d382b2d58
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-01-15 04:48:00 +0530

    [Farzana] Add: Python program to sort integer without conditional loops .

---
+++ Basic_Programs/20_SortInt.py
@@ -0,0 +1,12 @@
+"""
+@Author: Farzana Shaikh
+@Date: 12-01-2022 04:00AM
+@Last Modified by: Farzana Shaikh
+@Last Modified time: 12-01-2022 04:45AM
+@Title : Basic Python(To sort three integers without using conditional statements and loops.)
+"""
+
+if __name__ == "__main__":
+    list = [18, 2, 55, 3, 101]
+    print(sorted(list))
+    print(sorted(list, reverse=True))
