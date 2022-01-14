d205a73f6ae61e490b457a3d07ae9b096325938f
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-01-15 04:43:54 +0530

    [Farzana] Add: Python program to check whether the file exist or not.

---
+++ Basic_Programs/11_ChkFileExist.py
@@ -0,0 +1,13 @@
+"""
+@Author: Farzana Shaikh
+@Date: 12-01-2022 02:32AM
+@Last Modified by: Farzana Shaikh
+@Last Modified time: 12-01-2022 02:45AM
+@Title : Basic Python(To check whether a file exists.)
+"""
+
+import os.path
+
+if __name__ == "__main__":
+    print(os.path.isfile('8_HistogramPrint.py'))
+    print(os.path.exists('/home/user/Data-Structures/Data-Structures/BasicPrograms'))
