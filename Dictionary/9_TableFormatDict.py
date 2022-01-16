a9ddf44caa1a66affbe9312ec43420c318812de1
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-01-16 19:38:07 +0530

    [Farzana] Refactor: Refactored the function name.

--- Dictionary/9_TableFormatDict.py
+++ Dictionary/9_TableFormatDict.py
@@ -19,9 +19,6 @@ def iteration_of_dictionary_for_formatting():
         Return:
             Returns nothing but prints the dictionary.
     """
-    # Define the dictionary
-    dict = {}
-
     # Insert data into dictionary
     dict1 = {1: ["Samuel", 21, 'Data Structures'],
              2: ["Richie", 20, 'Machine Learning'],
@@ -36,30 +33,30 @@ def iteration_of_dictionary_for_formatting():
         name, age, course = value
         logging.debug("{:<10} {:<10} {:<10}".format(name, age, course))
 
-
-def zip_format_for_dictionary():
-    """
-        Description:
-            Function is used to print dictionary in table format.
-        Parameter:
-           No parameter required.
-        Return:
-            Returns nothing but prints the dictionary.
-    """
-    # define the dictionary
-    dict1 = {}
-
-    # insert data into dictionary.
-    dict1 = {'NAME': ['Samuel', 'Richie', 'Lauren'],
-             'AGE': [21, 20, 21],
-             'COURSE': ['Data Structures', 'Machine Learning', 'OOPS with Java']}
-
-    # print the contents using zip format.
-    for each_row in zip(*([i] + (j)
-                          for i, j in dict1.items())):
-        logging.debug(*each_row, " ")
+#
+# def zip_format_for_dictionary():
+#     """
+#         Description:
+#             Function is used to print dictionary in table format.
+#         Parameter:
+#            No parameter required.
+#         Return:
+#             Returns nothing but prints the dictionary.
+#     """
+#     # define the dictionary
+#     dict1 = {}
+#
+#     # insert data into dictionary.
+#     dict1 = {'NAME': ['Samuel', 'Richie', 'Lauren'],
+#              'AGE': [21, 20, 21],
+#              'COURSE': ['Data Structures', 'Machine Learning', 'OOPS with Java']}
+#
+#     # print the contents using zip format.
+#     for each_row in zip(*([i] + (j) for i, j in dict1.items())):
+#         logging.debug(*each_row, " ")
+#
 
 
 if __name__ == "__main__":
     iteration_of_dictionary_for_formatting()
-    zip_format_for_dictionary()
+    # zip_format_for_dictionary()
