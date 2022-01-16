98a2b38d192bea1c92b0bc3bdb31fd44f66fe66f
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-01-17 03:42:03 +0530

    [Farzana] Add: Python program to validate samples of EmailId.

---
+++ UserRegistration/UC9.py
@@ -0,0 +1,54 @@
+"""
+@Author: Farzana Shaikh
+@Date: 15-01-2022 04:00PM
+@Last Modified by: Farzana Shaikh
+@Last Modified time: 15-01-2022 04:00PM
+@Title : Python Program for user registration.
+"""
+
+from Logger import logger
+import re
+
+
+def main():
+    """
+       Description:
+           Function is used to take input from user.
+       Parameter:
+          No parameter required.
+       Return:
+           Returns nothing.
+    """
+    sample_emailid = ['abc@yahoo.com', 'abc-100@yahoo.com', 'abc.100@yahoo.com', 'abc111@abccom', 'abc-100@abc.net',
+                      'abc.100@abc.com.au', 'abc@1.com', 'abc@gmail.com.com', 'abc+100@gmail.com']
+    for i in sample_emailid:
+        regex(i)
+
+    # user_input = input('Enter Valid EmailId : ')
+    # regex(user_input)
+
+
+def regex(string):
+    """
+       Description:
+           Function is used to check whether the given input is valid or not.
+       Parameter:
+          String as a parameter required.
+       Return:
+           Returns nothing.
+    """
+    patt = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    matches = re.fullmatch(patt, string)
+    if matches:
+        logger.debug('Email-Id is Valid')
+        logger.info(f"Executed Successfully {string}")
+    else:
+        try:
+            raise Exception('Wrong Pattern Entered, should follow the Email-Id syntax')
+        except Exception as e:
+            print(e)
+            logger.error(e)
+
+
+if __name__ == '__main__':
+    main()
