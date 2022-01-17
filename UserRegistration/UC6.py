fc8b7fd1d5399c6f903b9e6d4bbecea0d471f941
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-01-17 15:29:41 +0530

    [Farzana] Refactor: Python program to take user's Password as an input and validate it for having minimum 8 characters and atleast 1 uppercase alphabet.

--- UserRegistration/UC6.py
+++ UserRegistration/UC6.py
@@ -19,7 +19,8 @@ def main():
        Return:
            Returns nothing.
     """
-    user_input = input('Enter Valid Password with minimum 8 character and atleast 1 uppercase: ')
+    logger.debug('Password must be of minimum 8 character and atleast contain 1 uppercase alphabet:')
+    user_input = input('Enter Valid Password: ')
     regex(user_input)
 
 
